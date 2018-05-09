c771e68a929356d765ecffcd75ae14f4ba0dbfb6
Richie Bendall <richiebendall@gmail.com>
2018-05-09 19:23:00 +1200

    Updated sw

--- sw.js
+++ sw.js
@@ -1,22 +1,11 @@
 var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/styles/404style.css',
-  '/styles/simplestyle.css',
-  '/scripts/script.js',
-  '/images/background.jpg',
-  '/images/favicon.png',
-  '/privacy',
-  '/terms',
+var urlsToCache = ['/',
+  '/styles/main.css', '/styles/404style.css', '/styles/simplestyle.css', '/scripts/script.js', '/images/background.jpg', '/images/favicon.png', '/privacy', '/terms'
 ];
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  }));
 });
