ea985bd7b72738e691f26f84f31af618db90ec97
Richie Bendall <richiebendall@gmail.com>
2018-08-05 21:54:07 +1200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
+  'index.html'
 ];
 
 // The install handler takes care of precaching the resources we always need.
