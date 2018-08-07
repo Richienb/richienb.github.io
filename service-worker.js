e08cc00e2317593ee0ab92b4a05fe75513d40a46
Richie Bendall <richiebendall@gmail.com>
2018-08-07 23:40:43 +1200

    Fix Service Worker [2]

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,60 @@
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+// This is the service worker with the Cache-first network
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-    'index.html';
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+    'index.html',
     'favicon.ico'
-];
+    ];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(PRECACHE)
-        .then(cache => cache.addAll(PRECACHE_URLS))
-        .then(self.skipWaiting())
-    );
-    // Show toast on installation success
-    r(function() {
-        var snackbarContainer = document.querySelector('#toast-object');
-        var data = {
-            message: 'Richie Bendall\'s Website is now available offline.'
-        };
-        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+// Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('Skip waiting on install');
+    return self.skipWaiting();
+  }));
+});
+
+
+// Allow sw to control of current page
+self.addEventListener('activate', function(event) {
+  console.log('Claiming clients for current page');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+function fromCache(request) {
+  // We pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
     });
+  });
+}
 
-    function r(f) {
-        /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f();
-    }
-});
+function update(request) {
+  // This is where we call the server to get the newest version of the 
+  // File to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-        }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }));
-        }).then(() => self.clients.claim())
-    );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-    // Skip cross-origin requests, like those for Google Analytics.
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return caches.open(RUNTIME).then(cache => {
-                    return fetch(event.request).then(response => {
-                        // Put a copy of the response in the runtime cache.
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
-                    });
-                });
-            })
-        );
-    }
-});
+function fromServer(request){
+  // This is the fallback if it is not in the cache to go to the server and get it
+  return fetch(request).then(function(response){ return response});
+}
