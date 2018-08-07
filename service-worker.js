29084fdc906654fc019617ae608f02e7d6ec04ae
Richie Bendall <richiebendall@gmail.com>
2018-08-08 00:12:10 +1200

    Another Service Worker Fix [2]

--- service-worker.js
+++ service-worker.js
@@ -1,60 +1,42 @@
-// This is the service worker with the Cache-first network
+var CACHE_NAME = 'static-cache';
+var urlsToCache = [
+  'index.html',
+  'favicon.ico'
+];
 
-var CACHE = 'pwabuilder-precache';
-var precacheFiles = [
-    'index.html',
-    'favicon.ico'
-    ];
-
-// Install stage sets up the cache-array to configure pre-cache content
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache().then(function() {
-    console.log('Skip waiting on install');
-    return self.skipWaiting();
-  }));
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-
-// Allow sw to control of current page
-self.addEventListener('activate', function(event) {
-  console.log('Claiming clients for current page');
-  return self.clients.claim();
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.'+ evt.request.url);
-  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
-  evt.waitUntil(update(evt.request));
-});
-
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(precacheFiles);
-  });
-}
-
-function fromCache(request) {
-  // We pull files from the cache first thing so we can show them fast
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
     });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
   });
-}
-
-function update(request) {
-  // This is where we call the server to get the newest version of the 
-  // File to use the next time we show view
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
-
-function fromServer(request){
-  // This is the fallback if it is not in the cache to go to the server and get it
-  return fetch(request).then(function(response){ return response});
 }
