7b776956c358579402d965c6ad4f7fe876a3a45b
Richie Bendall <richiebendall@gmail.com>
2018-08-13 23:45:54 +1200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,43 @@
-var CACHE_NAME = 'static-cache';
+const cacheName = 'v1';
 
-var urlsToCache = [
-    '/index.html',
-    '/hosted/material.indigo-red.min.css',
-    '/hosted/material.min.js',
-    '/offline.html',
-    '/favicon.ico',
-    '/service-worker.js'
-];
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            return response || fetchAndCache(event.request);
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    })
+  );
 });
 
-function fetchAndCache(url) {
-    return fetch(url)
-        .then(function(response) {
-            // Check if we received a valid response
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            return caches.open(CACHE_NAME)
-                .then(function(cache) {
-                    cache.put(url, response.clone());
-                    return response;
-                });
-        })
-        .catch(function(error) {
-            console.log('Request failed:', error);
-            return fetch('offline.html')
-                .then(function(response) {
-                    // Check if we received a valid response
-                    if (!response.ok) {
-                        throw Error(response.statusText);
-                    }
-                    return caches.open(CACHE_NAME)
-                        .then(function(cache) {
-                            cache.put(url, response.clone());
-                            return response;
-                        });
-                })
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // Make copy/clone of response
+        const resClone = res.clone();
+        // Open cahce
+        caches.open(cacheName).then(cache => {
+          // Add response to cache
+          cache.put(e.request, resClone);
         });
-}
+        return res;
+      })
+      .catch(err => caches.match(e.request).then(res => res))
+  );
+});
