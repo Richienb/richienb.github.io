a58aa0d1ec4470e905704ca636b8e32b8bbd989c
Richie Bendall <richiebendall@gmail.com>
2018-08-14 17:13:42 +1200

    Built sw config

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": ".",
+  "globPatterns": [
+    "**/*.{yml,html,icns,png,md,ico,js,css,eot,ttf,woff,woff2,txt,xml,webmanifest,jpg,svg}"
+  ],
+  "swDest": "richienb.github.io/service-worker.js"
+};
