40ff5336394172e7a393dca150bf3ed407e15f72
Richie Bendall <richiebendall@gmail.com>
2018-09-05 23:28:31 +1200

    Update workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "globDirectory": ".",
     "globPatterns": [
-        "**/*.*",
+        "**/*.{html,icns,png,ico,js,css,eot,ttf,woff,woff2,txt,xml,webmanifest,jpg,svg}"
     ],
     "swDest": "service-worker.js"
 };
