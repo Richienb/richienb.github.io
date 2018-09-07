1d4164d048d0bc545088e2d7717d0401ff11c945
Richie Bendall <richiebendall@gmail.com>
2018-09-07 21:09:58 +1200

    Beautified files

--- gulpfile.js
+++ gulpfile.js
@@ -6,46 +6,34 @@ var connect = require('gulp-connect');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
-const imagemin = require('gulp-imagemin');
-var svgmin = require('gulp-svgmin');
 
-gulp.task('svg', function () {
-    return gulp.src('*')
-        .pipe(svgmin())
-        .pipe(gulp.dest('dist/images'));
-});
- 
-gulp.task('image', () =>
-    gulp.src('resources/*.{jpg,png,gif}"')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/resources'))
-);
- 
 gulp.task('html', function() {
-  return gulp.src('*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+    return gulp.src('*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist'));
 });
- 
-gulp.task('css', function () {
+
+gulp.task('css', function() {
     gulp.src('*.css')
         .pipe(cssmin())
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function() {
-  gulp.src('*.js')
-  .pipe(uglify())
-  .pipe(concat('script.js'))
-  .pipe(gulp.dest('dist'))
+    gulp.src('*.js')
+        .pipe(uglify())
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('dist'))
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    root: '.',
-    livereload: true
-  })
+    connect.server({
+        root: '.',
+        livereload: true
+    })
 });
 
-gulp.task('ci', ['html', 'css', 'js', 'image', 'svg']);
+gulp.task('ci', ['html', 'css', 'js']);
 gulp.task('live', ['connect']);
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if ('ontouchstart' in document.documentElement) {
 var $ = require('https://cdn.jsdelivr.net/npm/jquery@3.3/dist/jquery.min.js');
 
 var channelID = "UCxOiLzcbFeW-Ayz_MbgWIXw";
-$.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D' + channelID, function (data) {
+$.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D' + channelID, function(data) {
     var link = data.items[0].link;
     var id = link.substr(link.indexOf("=") + 1);
     $("#youtube_video").attr("src", "https://www.youtube-nocookie.com/embed/" + id + "?showinfo=0&rel=0");
