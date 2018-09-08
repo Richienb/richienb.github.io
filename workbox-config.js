ebd74997e68d0284c07e62c01c8d48c1fa745b35
Richie Bendall <richiebendall@gmail.com>
2018-09-08 22:20:55 +1200

    Major overhaul

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    "globDirectory": ".",
-    "globPatterns": [
-        "**/*.{html,icns,png,ico,js,css,eot,ttf,woff,woff2,txt,xml,webmanifest,jpg,svg}"
-    ],
-    "swDest": "service-worker.js"
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.*"
+  ],
+  "swSrc": "src/service-worker.js",
+  "swDest": "dist/service-worker.js",
+  "globIgnores": [
+    "../workbox-config.js"
+  ]
 };
