da2a29c9b1e0e16927fbaf98245aa337c1504fd7
Richie Bendall <richiebendall@gmail.com>
2018-10-03 23:12:46 +1300

    More changes

--- newsrc/app.js
+++ newsrc/app.js
@@ -1,2 +1,12 @@
 import {MDCRipple} from '@material/ripple';
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+import {MDCDrawer} from "@material/drawer";
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+import {MDCTopAppBar} from "@material/top-app-bar";
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+});
