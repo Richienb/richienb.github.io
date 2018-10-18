d3ecec04867816b0b573e28abdcd8307d19b99cd
Richie Bendall <richiebendall@gmail.com>
2018-10-19 00:44:17 +1300

    Ignore config file

--- src/workbox-config.js
+++ src/workbox-config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  "globDirectory": ".",
-  "globPatterns": [
-    "**/*.*"
-  ],
-  "swSrc": "sw.js",
-  "swDest": "service-worker.js",
-  "globIgnores": [
-    "node_modules/**/*",
-    "+(workbox-|webpack.|postcss.)config.js",
-    "app.+(js|scss)",
-    "yarn.lock",
-    "package.json"
-  ]
+    globDirectory: ".",
+    globPatterns: ["**/*.*"],
+    swSrc: "sw.js",
+    swDest: "service-worker.js",
+    globIgnores: [
+        "node_modules/**/*",
+        "+(workbox-|webpack.|postcss.)config.js",
+        "app.+(js|scss)",
+        "yarn.lock",
+        "+(package|sandbox.config).json",
+        ".prettierrc"
+    ]
 };
