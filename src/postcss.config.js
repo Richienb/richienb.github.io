c66d87f3eb83013d99ff08e1eb9ff371ee2b020b
Richie Bendall <richiebendall@gmail.com>
2018-10-30 11:35:56 +1300

    Cssnano advanced preset

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     plugins: {
         "postcss-preset-env": { stage: 0 },
-        cssnano: {}
+        cssnano: {preset: ['advanced', {
+                autoprefixer: false
+            }]}
     }
 };
