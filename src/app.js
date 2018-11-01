fd20cdb16b963fb0bf9d4f97625cf6b8e3e2c117
Richie Bendall <richiebendall@gmail.com>
2018-11-01 23:16:03 +1300

    Remove unneeded semicolons because of babel

--- src/app.js
+++ src/app.js
@@ -1,66 +1,66 @@
-console.log('%cMy website is open source on GitHub! https://github.com/Richienb/richienb.github.io', 'text-shadow: -0.5px 0 black, 0 0.5px black, 0.5px 0 black, 0 -0.5px black; color: #f44336; font-size: 2.5em');
+console.log('%cMy website is open source on GitHub! https://github.com/Richienb/richienb.github.io', 'text-shadow: -0.5px 0 black, 0 0.5px black, 0.5px 0 black, 0 -0.5px black; color: #f44336; font-size: 2.5em')
 
 // Check if passive scrolling mode is supported
 if ('ontouchstart' in document.documentElement) {
     // Enable passive scrolling mode
     document.addEventListener('touchstart', function() {
-        return;
-    }, {passive: true});
+        return
+    }, {passive: true})
 }
 
 // Check if the googtrans cookie exists
 if (document.cookie.replace(/(?:(?:^|.*;\s*)googtrans\s*\=\s*([^;]*).*$)|^.*$/, "$1").split("/")[2] !== undefined) {
     // Set the value of the selector to the value of the cookie
-    document.getElementById("language-selector").value = document.cookie.replace(/(?:(?:^|.*;\s*)googtrans\s*\=\s*([^;]*).*$)|^.*$/, "$1").split("/")[2];
+    document.getElementById("language-selector").value = document.cookie.replace(/(?:(?:^|.*;\s*)googtrans\s*\=\s*([^;]*).*$)|^.*$/, "$1").split("/")[2]
 } else {
     // Set the value of the selector to the default, English
-    document.getElementById("language-selector").value = "en";
+    document.getElementById("language-selector").value = "en"
 }
 
 // Import MDC Drawer
-import {MDCDrawer} from "@material/drawer/index";
+import {MDCDrawer} from "@material/drawer/index"
 
 // Initialise the drawer
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'))
 
 // Import MDC Top App Bar
-import {MDCTopAppBar} from "@material/top-app-bar/index";
+import {MDCTopAppBar} from "@material/top-app-bar/index"
 
 // Initialise the top app bar
-const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'))
 
 // Set main content of top app bar
-topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.setScrollTarget(document.getElementById('main-content'))
 
 // Listen for an open menu button click
 topAppBar.listen('MDCTopAppBar:nav', () => {
 
     // Invert the open state of the drawer
-    drawer.open = !drawer.open;
-});
+    drawer.open = !drawer.open
+})
 
 // Listen for a website link click
 document.getElementById("websitelink").addEventListener("click", function() {
 
     // Close the drawer
-    drawer.open = false;
-});
+    drawer.open = false
+})
 
 // Import MDC Ripple
-import {MDCRipple} from '@material/ripple/index';
+import {MDCRipple} from "@material/ripple/index"
 
 // For each button, main card area and drawer item
 document.querySelectorAll('.content-card .mdc-card__actions .mdc-card__action-buttons .mdc-button, .content-card .mdc-card__primary-action, .mdc-drawer .mdc-drawer__content .mdc-list a.mdc-list-item').forEach(function(el) {
 
     // Initialise the ripple effect
-    MDCRipple.attachTo(el);
-});
+    MDCRipple.attachTo(el)
+})
 
 // Import MDC Select
-import {MDCSelect} from "@material/select/index";
+import {MDCSelect} from "@material/select/index"
 
 // Initialise language selector
-const select = new MDCSelect(document.querySelector('.mdc-select'));
+const select = new MDCSelect(document.querySelector('.mdc-select'))
 
 // List for a language chosen
 select.listen('change', () => {
@@ -69,8 +69,8 @@ select.listen('change', () => {
     document.cookie = `googtrans=/en/${select.value}; path=/`
 
     // Refesh the page to start the translation
-    location.reload();
-});
+    location.reload()
+})
 
 // Import html5shiv printshiv
-import 'html5shiv/dist/html5shiv-printshiv.js';
+import "html5shiv/dist/html5shiv-printshiv.js"
