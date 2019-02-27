894ffb6854db6f56ede4e12d0d083d792cd08adc
Richie Bendall <richiebendall@gmail.com>
2019-02-27 19:31:51 +1300

    Update workbox version

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 if (workbox) {
     console.debug("Yay! Workbox has loaded 🎉");
